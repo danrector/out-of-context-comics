febe9fce98c70bc603bea0aeff89b8a8300dd723
Daniel Rector <rector.danielk@gmail.com>
2017-07-04 15:19:18 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Out of Context Comics</h1>
+        </div>
+
+        <img src={'http://i.imgur.com/Ohuu7bI.jpg?1'} />
+        <img src={'http://i.imgur.com/dZoFSAo.jpg?1'} />
+        <img src={'http://i.imgur.com/tEQAwsG.jpg?1'} />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
