e5ed19589300b405e921feb31fad5bd778e54edc
Daniel Rector <rector.danielk@gmail.com>
2017-10-08 17:24:25 -0400

    imgur api request, get album images

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//postman request
+var request = require("request");
+
+var options = { method: 'GET',
+  url: 'https://api.imgur.com/3/album/LVea8/images',
+  headers:
+   { 'postman-token': 'dabb5d4d-a54b-5792-d996-9008a54d2df8',
+     'cache-control': 'no-cache',
+     authorization: 'Bearer 27cbb87ca73ffe8376ce68a8e8e180d9c22f24f8' } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
+
+
 class App extends Component {
+
+
+//render
   render() {
     return (
       <div className="App">
