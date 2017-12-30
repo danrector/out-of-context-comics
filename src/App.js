658a4104c90b9fde57a61b6f8c552ccc7db4e2d4
Daniel Rector <rector.danielk@gmail.com>
2017-12-30 16:20:12 -0500

    added alt props to images, removed logo import for react default logo (not used)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 //postman request
@@ -31,9 +30,9 @@ class App extends Component {
           <h1>Out of Context Comics</h1>
         </div>
         <div className="App-pictures">
-          <img src={'http://i.imgur.com/Ohuu7bI.jpg?1'} />
-          <img src={'http://i.imgur.com/dZoFSAo.jpg?1'} />
-          <img src={'http://i.imgur.com/tEQAwsG.jpg?1'} />
+          <img alt="comic panel" src={'http://i.imgur.com/Ohuu7bI.jpg?1'} />
+          <img alt="comic panel" src={'http://i.imgur.com/dZoFSAo.jpg?1'} />
+          <img alt="comic panel" src={'http://i.imgur.com/tEQAwsG.jpg?1'} />
         </div>
       </div>
     );
